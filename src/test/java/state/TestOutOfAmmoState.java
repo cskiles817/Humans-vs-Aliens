101b8f5871929b352af3394b279d3483ea980106
ConnorSkiles <cs8559@engr.ship.edu>
2024-11-27 23:10:54 -0500

    TestOutOfAmmoState.java updates

--- src/test/java/state/TestOutOfAmmoState.java
+++ src/test/java/state/TestOutOfAmmoState.java
@@ -4,23 +4,42 @@ import org.junit.Test;
 
 public class TestOutOfAmmoState
 {
+  Environment e = new Environment(10, 10);
+  MockWeapon weapon = new MockWeapon("Test weapon", 0);
+
 
   @Test
   public void testInitialization()
   {
-
+    OutOfAmmoState state = new OutOfAmmoState();
+    AIContext context = new AIContext(1, e);
+    context.setCurrentState(state);
+    assertEquals(state, context.getCurrentState());
   }
 
   @Test
   public void testReload()
   {
-
+    OutOfAmmoState state = new OutOfAmmoState();
+    AIContext context = new AIContext(1, e);
+    context.setCurrentState(state);
+    weapon.reload();
+    state.handle(context, weapon);
+    assertTrue(context.getCurrentState() instanceof ReadyState);
   }
 
   @Test
   public void testMoveToCorrectState()
   {
-
+    OutOfAmmoState state = new OutOfAmmoState();
+    AIContext context = new AIContext(1, e);
+    context.setCurrentState(state);
+    weapon.reload();
+    state.handle(context, weapon);
+    assertTrue(context.getCurrentState() instanceof ReadyState);
+    weapon.useAmmo();
+    state.handle(context, weapon);
+    assertTrue(context.getCurrentState() instanceof OutOfAmmoState);
   }
 
   @Test

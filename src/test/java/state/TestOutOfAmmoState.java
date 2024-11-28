fcaab47ca165dc26a2f61fbb3b99a7cd577f873f
ConnorSkiles <cs8559@engr.ship.edu>
2024-11-27 22:57:28 -0500

    TestOutOfAmmoState.java changes

--- src/test/java/state/TestOutOfAmmoState.java
+++ src/test/java/state/TestOutOfAmmoState.java
@@ -2,25 +2,35 @@ package state;
 
 import org.junit.Test;
 
-public class TestOutOfAmmoState {
+public class TestOutOfAmmoState
+{
 
   @Test
-  public void testInitialization() {
+  public void testInitialization()
+  {
 
   }
 
   @Test
-  public void testReload() {
+  public void testReload()
+  {
 
   }
 
   @Test
-  public void testMoveToCorrectState() {
+  public void testMoveToCorrectState()
+  {
 
   }
 
   @Test
-  public void testIfDead() {
-
+  public void testIfDead()
+  {
+    MockLifeForm l = new MockLifeForm("Mock", 1);
+    e.addLifeForm(l, 1, 1);
+    AIContext context = new AIContext(1, e);
+    l.setCurrentLifePoints(0);
+    context.execute();
+    assertEquals(context.getDeadState(), context.getCurrentState());
   }
 }
